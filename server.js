3517913fea5c477478126804921458aa33f020d9
Oaldimshki <130537003+Oaldimshki@users.noreply.github.com>
2023-07-12 19:01:46 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const fetch = require('node-fetch');
 app.use(express.json()) //pass json from frontend to backend
 app.use(cors())
 
